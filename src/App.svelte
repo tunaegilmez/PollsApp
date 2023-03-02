4a511cda6f8a9d92895dea1630acfeaceeba5186
tunaegilmez <tunaegilmez07@gmail.com>
2023-03-03 01:49:51 +0300

    Casting Votes

--- src/App.svelte
+++ src/App.svelte
@@ -30,13 +30,29 @@
     console.log(polls);
     activeItem = "Current Polls";
   };
+
+  const handleVote = e => {
+    const { id, option } = e.detail;
+    console.log(e.detail);
+    let copiedPolls = [...polls];
+    let upvotedPoll = copiedPolls.find(poll => poll.id === id);
+
+    if (option === "a") {
+      upvotedPoll.votesA++;
+    }
+    if (option === "b") {
+      upvotedPoll.votesB++;
+    }
+
+    polls = copiedPolls;
+  };
 </script>
 
 <Header />
 <main>
   <Tabs {activeItem} {items} on:tabChange={tabChange} />
   {#if activeItem === "Current Polls"}
-    <PollList {polls} />
+    <PollList {polls} on:vote={handleVote} />
   {:else if activeItem === "Add New Poll"}
     <CreatePollForm on:add={handleAdd} />
   {/if}
--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,20 +1,28 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import Card from "../shared/Card.svelte";
   export let poll;
 
+  const dispatch = createEventDispatcher();
+
   // Reactive Values
   $: totalVotes = poll.votesA + poll.votesB;
+
+  // Handling Votes
+  const handleVote = (option, id) => {
+    dispatch("vote", { option, id });
+  };
 </script>
 
 <Card>
   <div class="poll">
     <h3>{poll.question}</h3>
     <p>Total Votes: {totalVotes}</p>
-    <div class="answer">
+    <div class="answer" on:click={() => handleVote("a", poll.id)}>
       <div class="percent percent-a" />
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
-    <div class="answer">
+    <div class="answer" on:click={() => handleVote("b", poll.id)}>
       <div class="percent percent-b" />
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
