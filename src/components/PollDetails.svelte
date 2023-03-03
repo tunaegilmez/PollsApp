acc41271d0d693933a4904e3cb042688f315fe02
tunaegilmez <tunaegilmez07@gmail.com>
2023-03-03 14:21:25 +0300

    transition, animate and motion

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -2,13 +2,21 @@
   import PollStore from "../stores/PollStore";
   import Card from "../shared/Card.svelte";
   import Button from "../shared/Button.svelte";
+  import { tweened } from "svelte/motion";
 
   export let poll;
 
   // Reactive Values
   $: totalVotes = poll.votesA + poll.votesB;
-  $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
-  $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
+  $: percentA = Math.floor((100 / totalVotes) * poll.votesA) || 0;
+  $: percentB = Math.floor((100 / totalVotes) * poll.votesB) || 0;
+
+  // tweened percentages
+  const tweenedA = tweened(0);
+  const tweenedB = tweened(0);
+  $: tweenedA.set(percentA);
+  $: tweenedB.set(percentB);
+  // $: console.log($tweenedA, $tweenedB);
 
   // Handling Votes
   const handleVote = (option, id) => {
@@ -40,11 +48,11 @@
     <h3>{poll.question}</h3>
     <p>Total Votes: {totalVotes}</p>
     <div class="answer" on:click={() => handleVote("a", poll.id)}>
-      <div class="percent percent-a" style="width:{percentA}%" />
+      <div class="percent percent-a" style="width:{$tweenedA}%" />
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <div class="answer" on:click={() => handleVote("b", poll.id)}>
-      <div class="percent percent-b" style="width:{percentB}%" />
+      <div class="percent percent-b" style="width:{$tweenedB}%" />
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
     <div class="delete">
--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,11 +1,13 @@
 <script>
+  import { fade, slide, scale } from "svelte/transition";
+  import { flip } from "svelte/animate";
   import PollStore from "../stores/PollStore.js";
   import PollDetails from "./PollDetails.svelte";
 </script>
 
 <div class="poll-list">
   {#each $PollStore as poll (poll.id)}
-    <div>
+    <div in:fade out:scale|local animate:flip={{ duration: 500 }}>
       <PollDetails {poll} />
     </div>
   {/each}
