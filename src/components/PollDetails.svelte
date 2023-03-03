88ca968a909135b2b9b134a94b6443a03af13f2c
tunaegilmez <tunaegilmez07@gmail.com>
2023-03-03 13:45:38 +0300

    delete button

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,6 +1,8 @@
 <script>
   import PollStore from "../stores/PollStore";
   import Card from "../shared/Card.svelte";
+  import Button from "../shared/Button.svelte";
+
   export let poll;
 
   // Reactive Values
@@ -24,6 +26,13 @@
       return copiedPolls;
     });
   };
+
+  // deleteing poll
+  const handleDelete = id => {
+    PollStore.update(currentPolls => {
+      return currentPolls.filter(poll => poll.id !== id);
+    });
+  };
 </script>
 
 <Card>
@@ -37,6 +46,9 @@
     <div class="answer" on:click={() => handleVote("b", poll.id)}>
       <div class="percent percent-b" style="width:{percentB}%" />
       <span>{poll.answerB} ({poll.votesB})</span>
+    </div>
+    <div class="delete">
+      <Button flat={true} on:click={() => handleDelete(poll.id)}>Delete</Button>
     </div>
   </div>
 </Card>
@@ -79,5 +91,9 @@
     border-left: 4px solid #45c496;
     background: rgba(69, 196, 150, 0.2);
     /* width: 75%; */
+  }
+  .delete {
+    margin-top: 30px;
+    text-align: right;
   }
 </style>
--- src/shared/Button.svelte
+++ src/shared/Button.svelte
@@ -4,7 +4,7 @@
   export let inverse = false;
 </script>
 
-<button class={type} class:flat class:inverse>
+<button class={type} class:flat class:inverse on:click>
   <slot />
 </button>
 
